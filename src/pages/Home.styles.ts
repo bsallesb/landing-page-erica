db077cce61276d19c06d708371161511c820b6e8
Breno <osalesbreno@gmail.com>
2025-04-01 18:55:35 -0300

    chore: adjusting background size

--- src/pages/Home.styles.ts
+++ src/pages/Home.styles.ts
@@ -5,15 +5,17 @@ export const Main = styled.main``;
 
 export const Section = styled.section`
   background-image: url(${backgroundTooth});
-  background-size: 8%;
+  background-size: 15%;
   background-repeat: repeat;
   height: 103px;
 
   @media (max-width: 700px) {
     height: 82.55px;
+    background-size: 25%;
   }
 
   @media (max-width: 600px) {
     height: 68.78px;
+    background-size: 30%;
   }
 `;
